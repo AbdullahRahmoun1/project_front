5ee37bade0e4e5a5e25e1d9332ba482964777bd8
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2023-01-08 16:35:07 +0300

    Move Requests To Server.dart

--- lib/server/server.dart
+++ lib/server/server.dart
@@ -1,34 +1,54 @@
+import 'package:consulting_app/server/auth.dart';
 import 'dart:convert';
-import 'dart:js';
-
-import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:collection';
+import 'package:flutter/cupertino.dart';
 
-class Server with ChangeNotifier{
-  String? token;
-  static final routName="/server";
+class Server with ChangeNotifier {
+  final String baseUrl='127.0.0.1';
 
-  Future<Map> singIn(String phone, String password) async {
-    final result = {};
-    try {
-      final url = Uri.parse('uri');
-      final response = await http.post(url,
-          headers: {'accept': 'application/json', 'authorization': ''},
-          body: json.encode({'phone': phone, 'password': password}));
-      var body = json.decode(response.body);
-      if (response.statusCode == 404) {
-        var msg = body['msg'];
-      }
-      //error
-    } catch (error) {
-      print(error);
-    }
-    return result;
+  String? _getToken(context) {
+    return Provider.of<Auth>(context, listen: false).token;
   }
-  Future<Map> uploadImage() async{
-    var result;
 
-    return result;
+  Future<void> FetchCategory(items, context) async {
+    try {
+      final url = Uri.parse('http://$baseUrl:8000/api/home');
+      var token = _getToken(context);
+      Map<String, String> header = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      };
+      final response = await http.get(url, headers: header);
+      final extractData =
+          json.decode(response.body) as LinkedHashMap<String, dynamic>;
+      for (var i = 0; i < items.length; i++) {
+        items[i].name = extractData['data']['Specialities'][i]['specialtyName'];
+      }
+      print(response.statusCode);
+      print(json.decode(response.body));
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<Map<String, dynamic>> getUserData(int? id, context) async {
+    final url = Uri.parse('http://$baseUrl:8000/api/user/-1');
+    var token = _getToken(context);
+    Map<String, String> header = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+    final response = await http.get(url, headers: header);
+    final extraxtData1 = json.decode(response.body) as Map<String, dynamic>;
+    dynamic extraxtData = Map<String, dynamic>;
+    print( extraxtData1['data']['phone']);
+    return {
+      'name': extraxtData1['data']['name'],
+      'phone': extraxtData1['data']['phone'],
+      'isExp': extraxtData1['data']['isExp']
+    };
   }
 }
